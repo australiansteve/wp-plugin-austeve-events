5547f9df1d1d721aa74ba7cd15669745aabd2e6d
australiansteve <steve@australiansteve.com>
2016-04-05 22:22:12 -0300

    Initial commit

---
+++ austeve-project-showcase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Project Showcase
+ * Plugin URI: https://github.com/australiansteve/wp-plugin-austeve-project-showcase
+ * Description: Showcase a portfolio of projects
+ * Version: 0.0.1
+ * Author: AustralianSteve
+ * Author URI: http://AustralianSteve.com
+ * License: GPL2
+ */
+
+include( plugin_dir_path( __FILE__ ) . 'admin.php');
+include( plugin_dir_path( __FILE__ ) . 'widget.php');
+
+?>
---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+// Load our plugins
+var	gulp			=	require('gulp'),
+	sass			=	require('gulp-sass'),  // Our sass compiler
+	notify			=	require('gulp-notify'), // Basic gulp notificatin using OS
+	minifycss		=	require('gulp-minify-css'), // Minification
+	rename			=	require('gulp-rename'), // Allows us to rename our css file prior to minifying
+	autoprefixer	=	require('gulp-autoprefixer'), // Adds vendor prefixes for us
+	concat			= require('gulp-concat'), // Concat our js
+	uglify			= require('gulp-uglify'), // Minify our js
+	jshint 			= require('gulp-jshint'); // Checks for js errors
+
+
+// Our 'styles' tasks, which handles our sass actions such as compliling and minification
+
+gulp.task('styles', function() {
+		gulp.src('./style.scss')
+		.pipe(sass({
+			style: 'expanded',
+			sourceComments: true
+		})
+		.on('error', notify.onError(function(error) {
+			return "Error: " + error.message;
+		}))
+		)
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', 'ie >= 8']
+		})) // our autoprefixer - add and remove vendor prefixes using caniuse.com
+		.pipe(gulp.dest('.')) // Location of our style.css file
+		.pipe(notify({
+			message: "Styles task complete!"
+		}));
+});
+
+//Our 'deploy' task which deploys on a local dev environment
+
+gulp.task('deploylocal', function() {
+
+	var files = [
+		'js/**/*.js',
+		'*.php',
+		'*.css'];
+
+	var dest = '/var/www/html/theme-dev/wp-content/plugins/austeve-project-showcase';
+
+	return gulp.src(files, {base:"."})
+	        .pipe(gulp.dest(dest));
+});
+
+// Our default gulp task, which runs all of our tasks upon typing in 'gulp' in Terminal
+gulp.task('default', ['styles', 'deploylocal']);
